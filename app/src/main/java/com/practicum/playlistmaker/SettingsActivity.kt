98e0e64b391fc002308e41e038c56b59eedb2eaf
Dmitry1 <kallisto66@mail.ru>
2025-05-27 15:03:39 +0300

    Спринт 9

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
@@ -16,6 +17,34 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val backButton = findViewById<MaterialToolbar>(R.id.toolbarSetting)
+        val shareIcon = findViewById<ImageView>(R.id.shareButton)
+        val helpIcon = findViewById<ImageView>(R.id.helpButton)
+        val agreementIcon = findViewById<ImageView>(R.id.agreementButton)
+
+        val mailMessage = "Спасибо разработчикам и разработчицам за крутое приложение!"
+        val mailTe = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+
+        shareIcon.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.message))
+            startActivity(shareIntent)
+        }
+
+        helpIcon.setOnClickListener {
+            val helpIntent = Intent(Intent.ACTION_SENDTO)
+            helpIntent.data = Uri.parse("mailto:")
+            helpIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("perovdv1@ya.ru"))
+            helpIntent.putExtra(Intent.EXTRA_SUBJECT, mailTe)
+            helpIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.mailMessage))
+            startActivity(helpIntent)
+        }
+
+        agreementIcon.setOnClickListener {
+            val url = "https://yandex.ru/legal/practicum_offer/"
+            val agreeIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(agreeIntent)
+        }
 
         backButton.setNavigationOnClickListener {
             finish()
