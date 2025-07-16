0b961d47af4386926c10771cf9f646d5224cf9e2
Dmitry1 <kallisto66@mail.ru>
2025-07-16 18:03:39 +0300

    Спринт 12

---
+++ app/src/main/java/com/practicum/playlistmaker/data/storage/SearchHistory.kt
@@ -0,0 +1,48 @@
+package com.practicum.playlistmaker.data.storage
+
+import android.annotation.SuppressLint
+import android.content.SharedPreferences
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.practicum.playlistmaker.data.network.request.Track
+
+class SearchHistory(private val sharedPrefs: SharedPreferences) {
+
+    companion object {
+        private const val KEY_SEARCH_HISTORY = "search_history"
+        private const val MAX_SIZE = 10
+    }
+
+    fun saveHistory(track: List<Track>) {
+        val json = Gson().toJson(track)
+        sharedPrefs.edit()
+            .putString(KEY_SEARCH_HISTORY,json)
+            .apply()
+    }
+
+    fun getHistory(): List<Track> {
+        val json = sharedPrefs.getString(KEY_SEARCH_HISTORY, null) ?: return emptyList()
+        val type = object: TypeToken<List<Track>>() {}.type
+        return Gson().fromJson(json, type)
+    }
+
+    fun clearTrackHistory() {
+        sharedPrefs.edit()
+            .remove(KEY_SEARCH_HISTORY)
+            .apply()
+    }
+
+    @SuppressLint("NewApi")
+    fun addTrackHistory(track: Track){
+        val historyList = getHistory().toMutableList().apply() {
+
+            removeAll { it.trackId == track.trackId }
+            add(0,track)
+            if (size > MAX_SIZE) {
+                removeLast()
+            }
+        }
+        saveHistory(historyList)
+    }
+}
--- app/src/main/java/com/practicum/playlistmaker/presentation/setting/App.kt
+++ app/src/main/java/com/practicum/playlistmaker/presentation/setting/App.kt
@@ -9,10 +9,27 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        darkTheme = getSharedPreferences("settings", MODE_PRIVATE)
+            .getBoolean("dark_theme", false)
+
+        AppCompatDelegate.setDefaultNightMode(
+            if(darkTheme) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+        )
     }
 
     fun switchTheme(darkThemeEnabled: Boolean) {
         darkTheme = darkThemeEnabled
+
+        getSharedPreferences("settings", MODE_PRIVATE)
+            .edit()
+            .putBoolean("dark_theme", darkThemeEnabled)
+            .apply()
+
         AppCompatDelegate.setDefaultNightMode(
             if(darkThemeEnabled){
                 AppCompatDelegate.MODE_NIGHT_YES
