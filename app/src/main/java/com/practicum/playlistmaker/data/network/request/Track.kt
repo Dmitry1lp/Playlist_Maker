4caab1e393f12b2607a98473938e19224eba96e7
Dmitry1 <kallisto66@mail.ru>
2025-07-24 16:05:51 +0300

    Спринт 13

--- app/src/main/java/com/practicum/playlistmaker/data/network/request/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/data/network/request/Track.kt
@@ -1,9 +1,17 @@
 package com.practicum.playlistmaker.data.network.request
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Track (
-    val trackId: Long,
-    val trackName: String, // Название композиции
-    val artistName: String, // Имя исполнителя
+    val trackId: Long, //ID
+    val collectionName: String?, //Название альбома
+    val releaseDate: String?, // Год
+    val primaryGenreName: String?, //Жанр
+    val country: String?, //Страна
+    val trackName: String?, // Название композиции
+    val artistName: String?, // Имя исполнителя
     val trackTime: String, // Продолжительность трека
-    val artworkUrl100: String // Ссылка на изображение обложки
-)
+    val artworkUrl100: String? // Ссылка на изображение обложки
+): Parcelable
